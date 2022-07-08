52fbb6876b199ae74afd45687f77b1e9b6f3fffd
Norton Rayan <norton_rayan@hotmail.com>
2022-07-08 16:55:35 -0300

    feat: rate

--- src/components/listItem/index.tsx
+++ src/components/listItem/index.tsx
@@ -1,10 +1,12 @@
-import { Container, Thumbnail, Title, TitleContainer } from './styles';
-
+import { Container, Icon, Rate, RateContainer, Thumbnail, Title, TitleContainer } from './styles';
+import StarSVG from "../../assets/star.svg";
 type props = {
     item: any
 }
 
 export function ListItem({ item }: props) {
+
+    console.log(1);
 
     const imageSource = item.image?.original ? item.image.original : "https://i.pinimg.com/originals/f4/0e/e2/f40ee225e3dccda7aa1a4de202b241bf.jpg";
 
@@ -14,6 +16,15 @@ export function ListItem({ item }: props) {
             <TitleContainer>
                 <Title>{item.name}</Title>
             </TitleContainer>
+            
+            {
+                item.rating.average &&
+                <RateContainer>
+                    <Icon src={StarSVG} />
+                    <Rate>{item.rating.average}</Rate>
+                </RateContainer>
+            }
+
         </Container>
     )
 }
